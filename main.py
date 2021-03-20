9b5f8dab7249356be88c27e2b4f7a3c4c73f744c
NearBirdEZ <ytfhbbhl@yandex.ru>
2021-03-20 16:24:12 +0300

    hot fix write url

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ class UnloadFns:
         for inn, factory_number_kkt, rnm, replace_date, *fn_list in self.connect.sql_select(sql_req):
 
             if replace_date and (
-                    dt.datetime.utcfromtimestamp(self.date_list[0]) > replace_date < dt.datetime.utcfromtimestamp(
+                    dt.datetime.utcfromtimestamp(self.date_list[0]) < replace_date < dt.datetime.utcfromtimestamp(
                     self.date_list[1])):
                 fn_list = fn_list[:-1]
 
