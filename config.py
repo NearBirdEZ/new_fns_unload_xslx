721deedb0de2f21579eec31c37c76b6dba73fdd7
NearBirdEZ <ytfhbbhl@yandex.ru>
2021-03-31 14:58:03 +0300

    add version

--- config.py
+++ config.py
@@ -1,17 +1,20 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+os.chdir(basedir)
 
 
 class Config(object):
     """ИНФОРМАЦИЯ ПО ПРОМО ДБ"""
-    NAME_DATABASE_PROM = ''
-    USER_DB_PROM = ''
-    PASSWORD_DB_PROM = ''
-    HOST_DB_PROM = ''
-    PORT_DB_PROM = ''
+    NAME_DATABASE_PROM = os.environ.get('NAME_DATABASE_PROM') or ''
+    USER_DB_PROM = os.environ.get('USER_DB_PROM') or ''
+    PASSWORD_DB_PROM = os.environ.get('PASSWORD_DB_PROM') or ''
+    HOST_DB_PROM = os.environ.get('HOST_DB_PROM') or ''
+    PORT_DB_PROM = os.environ.get('PORT_DB_PROM') or ''
 
-    USER_EL_PROM =''
-    PASSWORD_EL_PROM = ''
-    HOST_EL_PROM = ''
-    PORT_EL_PROM = ''
+    USER_EL_PROM = os.environ.get('USER_EL_PROM') or ''
+    PASSWORD_EL_PROM = os.environ.get('PASSWORD_EL_PROM') or ''
+    HOST_EL_PROM = os.environ.get('HOST_EL_PROM') or ''
+    PORT_EL_PROM = os.environ.get('PORT_EL_PROM') or ''
+
+    local_version = 0.1
